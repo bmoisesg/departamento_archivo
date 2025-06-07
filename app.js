c6aa551fad51852da51b714d24187f9b6c9e50fa
bmoisesg <bmoisesg@gmail.com>
2025-06-07 12:53:27 -0600

    feat:estado actual de un archivo

--- app.js
+++ app.js
@@ -100,3 +100,19 @@ app.get('/estados_archivo', async(req, res) => {
         res.status(500).json({ error: 'Error al conectar a la base de datos' });
     }
 });
+
+
+app.get('/estado_archivo/:id', async(req, res) => {
+    try {
+        let id = req.params.id;
+        const pool = await sql.connect(config);
+        const result = await pool.request()
+            .input('id_archivo', sql.Int, id)
+            .query('EXEC sp_estadoActualArchivo @id_archivo');
+
+        res.json(result.recordset);
+    } catch (err) {
+        console.error('Error en la consulta:', err);
+        res.status(500).json({ error: 'Error al conectar a la base de datos' });
+    }
+});
